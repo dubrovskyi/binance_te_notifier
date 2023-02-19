bbdd38dabb079cdbf54a69d585f2f465d3e2cad6
vova <volodymyr.dubovskyi@hpe.com>
2023-02-19 19:44:12 +0200

    change

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def send_message(message):
 
 
 # defining key/request url
-async def main(pair='HFTUSDT', dist=0.1, timeout=10) -> None:
+async def main(pair='HFTUSDT') -> None:
     """
     Binance notifier with symbol pair to Telegram by dist and timeout
     :param pair: str symbol value by which pair need to check on Binance price
@@ -28,30 +28,31 @@ async def main(pair='HFTUSDT', dist=0.1, timeout=10) -> None:
     :param timeout: how frequently need to ping binance server
     :return: None
     """
-    price_for_notification = 0
     while True:
-        key = f"https://api.binance.com/api/v3/ticker/price?symbol={pair}"
+        try:
+            key = f"https://api.binance.com/api/v3/ticker/price?symbol={pair}"
 
-        # requesting data from url
-        data = requests.get(key)
-        data = data.json()
-        if value := float(data['price']):
-            if (price_for_notification + float(dist)) < value:
-                price_for_notification = value
-                await send_message(f'{pair} \U0001F4B9 {price_for_notification}')
-            elif (price_for_notification - float(dist)) > value:
-                price_for_notification = value
-                await send_message(f'{pair} \U0001F53B {price_for_notification}')
-        sleep(int(timeout))
+            # requesting data from url
+            data = requests.get(key)
+            data = data.json()
+            if value := float(data['price']):
+                # if (price_for_notification + 0.05) < value:
+                if value > 0.9:
+                    # price_for_notification = value
+                    await send_message(f'{pair} \U0001F4B9 {value}')
+                # elif (price_for_notification - 0.05) > value:
+                elif value < 0.7:
+                    # price_for_notification = value
+                    await send_message(f'{pair} \U0001F53B {value}')
+            sleep(10)
+
+        except Exception:
+            logger.info('ConnectionError appeared')
+            time.sleep(connection_error_timeout)
 
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    try:
-        asyncio.run(main(sys.argv[1] if len(sys.argv) > 1 else 'HFTUSDT'))
-    except KeyboardInterrupt:
-        pass
-    except requests.exceptions.ConnectionError:
-        logger.info('ConnectionError appeared')
-        time.sleep(connection_error_timeout)
+    asyncio.run(main(sys.argv[1] if len(sys.argv) > 1 else 'HFTUSDT'))
+
