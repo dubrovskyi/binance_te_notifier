3dfb7cf3bb846ef4cfdbe6d10391a544824ba0b5
vova <volodymyr.dubovskyi@hpe.com>
2023-02-20 10:33:55 +0200

    decompose main function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import asyncio
 import logging
 import os
 import sys
-import time
 from time import sleep
-import asyncio
 
 from telegram import Bot
 import requests
@@ -11,12 +10,19 @@ import requests
 chat_id = os.getenv('CHAT_ID', '')
 bot_token = os.getenv('BOT_TOKEN', '')
 connection_error_timeout = int(os.getenv('ERROR_TIMEOUT', 600))
+bot = Bot(token=bot_token)
 logger = logging.getLogger(__name__)
 
 
 async def send_message(message):
-    bot = Bot(token=bot_token)
     await bot.send_message(chat_id=chat_id, text=message)
+
+
+def check_symbol_price(symbol):
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+    data = requests.get(url)
+    data = data.json()
+    return float(data['price'])
 
 
 # defining key/request url
@@ -24,32 +30,27 @@ async def main(pair='HFTUSDT') -> None:
     """
     Binance notifier with symbol pair to Telegram by dist and timeout
     :param pair: str symbol value by which pair need to check on Binance price
-    :param dist: float value when need to notify if previous value changed on
-    :param timeout: how frequently need to ping binance server
+    # :param dist: float value when need to notify if previous value changed on
+    # :param timeout: how frequently need to ping binance server
     :return: None
     """
     price_for_notification = 0
     while True:
         try:
-            key = f"https://api.binance.com/api/v3/ticker/price?symbol={pair}"
-
-            # requesting data from url
-            data = requests.get(key)
-            data = data.json()
-            if value := float(data['price']):
-                if (price_for_notification + 0.01) <= value:
-                # if value > 0.9:
-                    price_for_notification = value
-                    await send_message(f'{pair} \U0001F4B9 {value}')
-                elif (price_for_notification - 0.01) >= value:
-                # elif value < 0.7:
-                    price_for_notification = value
-                    await send_message(f'{pair} \U0001F53B {value}')
+            value = check_symbol_price(pair)
+            if (price_for_notification + 0.01) <= value:
+            # if value > 0.9:
+                price_for_notification = value
+                await send_message(f'{pair} \U0001F4B9 {value}')
+            elif (price_for_notification - 0.01) >= value:
+            # elif value < 0.7:
+                price_for_notification = value
+                await send_message(f'{pair} \U0001F53B {value}')
             sleep(60)
 
-        except Exception:
-            logger.info('ConnectionError appeared')
-            time.sleep(connection_error_timeout)
+        except Exception as e:
+            logger.info(f'Exception appeared {e}')
+            sleep(connection_error_timeout)
 
 
 if __name__ == '__main__':
